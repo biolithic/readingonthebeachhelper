686f6d98bf419bc270bcbbffdd012a665184036b
Andy Martha <andrewpmartha@gmail.com>
2014-02-14 22:50:43 -0600

    formatted modules to d.o specs

--- startreading.js
+++ startreading.js
@@ -1,22 +1,25 @@
 (function ($) {
   Drupal.behaviors.interact = {
     attach: function (context, settings) {
-$('h2 a').each(function() {
-$(this).attr("href", "");
-});
 
-var myArray = [];
-$("a[title='open this book']").each(function() {
-myArray.push($(this).attr("href"));
-});
+// for each chapter title, we don't want the user to click the chapter title
+    $('h2 a').each(function() {
+      $(this).attr("href", "");
+      });
 
-var i = 0;
-$("img").each(function( index ) {
-var imagedomnumber = index;
-$(this).bind ("click", function(){
-location.replace(myArray[imagedomnumber]);
-});
-});
+// when the user clicks on a book cover, take them to the table of contents of that book
+    var myArray = [];
+    $("a[title='open this book']").each(function() {
+    myArray.push($(this).attr("href"));
+      });
+
+    var i = 0;
+    $("img").each(function( index ) {
+    var imagedomnumber = index;
+    $(this).bind ("click", function(){
+      location.replace(myArray[imagedomnumber]);
+      });
+    });
 
 }
 }
